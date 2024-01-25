45f0c45e81d5a6ede721dd26f924175bb4e35ba1
Jan Safar <jan.safar@gla-rad.org>
2024-01-25 12:18:53 +0000

    Added a copyright statement. Added pytest as a dev dependency.

--- src/iec_pas_63343/sentences.py
+++ src/iec_pas_63343/sentences.py
@@ -6,11 +6,23 @@ This module contains classes and functions for representing, generating [and
 parsing] presentation interface sentences compliant with the draft
 IEC PAS 63343, dated Oct 2020.
 
-Created on Thu Dec  2 16:59:04 2021
-
 @author: Jan Safar
-"""
 
+Copyright 2024 GLA Research and Development Directorate
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
 # =============================================================================
 # %% Import Statements
 # =============================================================================
